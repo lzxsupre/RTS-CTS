8291c264e37ddc26fe0e0b7738e1a38ee2669ef0
lzxsupre <1322580533@qq.com>
2022-10-16 11:31:45 +0800

    fix

--- send_RTS.py
+++ send_RTS.py
@@ -2,7 +2,7 @@ import datetime
 from scapy.all import *
 from scapy.all import Dot11,Dot11Beacon,Dot11Elt,RadioTap,sendp
 
-ifacx="Qualcomm Atheros QCA9377 Wireless Network Adapter"
+ifacx="Qualcomm Atheros QCA9377 Wireless Network Adapter"    #localhost WLAN-interface
 
 targetad='c0:1c:30:1a:63:a9' #target mac_address
 myad='20:4e:f6:fa:ca:a9'    #localhost mac_address
